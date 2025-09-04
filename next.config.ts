ff092731c7c48afddaec4a9e6166b5cefa25da53
Cyrus Sepehr <cyrusseper@gmail.com>
2025-09-04 00:21:33 -0500

    Use minimal Next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Do not set output: 'export'
+  // Do not disable the app router
 };
 
 export default nextConfig;
